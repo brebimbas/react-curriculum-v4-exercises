46554a71e48de18ebc6119e0d15c1522c1e49eb5
Brian Rebimbas <brianrebimbas@Mac.T-mobile.com>
2026-07-22 19:05:46 -0400

    Complete Lesson 3 Excercise

--- src/exercises/lesson-03/BugMutatedState.jsx
+++ src/exercises/lesson-03/BugMutatedState.jsx
@@ -10,11 +10,10 @@
 
 import { useState } from 'react';
 export default function BugMutatedState() {
-  let [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
   function handleAdd() {
-    count++;
-    setCount(count);
+    setCount((count) => count + 1);
   }
 
   return (
@@ -26,4 +25,6 @@ export default function BugMutatedState() {
 }
 
 // Explanation:
-// (Write your explanation here)
+// I removed count++ because it directly mutates count. State should be treated immutable
+// Change to a const
+// Using setCount(count => count + 1) react recieves a new reference and triggers a re-render
